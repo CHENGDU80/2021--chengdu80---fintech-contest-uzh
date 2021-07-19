f0cfd2729152542a90a319766135dce63ea650af
Zhifei Yang <xividyzf@gmail.com>
2021-07-19 01:02:19 +0000

    basic sb-admin

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return render_template('sb-admin/index.html')
+#    return "<h1>Hello, World!</h1>"
 
 if __name__ == "__main__":
     app.run()
